48ffbc10348b6d8e064c0fd1e46db2243812c218
pranjul29 <paras.paharia29@gmail.com>
2021-02-14 16:38:31 +0530

    Added Card Component for HomePage

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,29 +1,174 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Header from "../../common/header/Header";
+import Grid from '@material-ui/core/Grid';
+import './Home.css';
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import 'font-awesome/css/font-awesome.min.css';
+import '@fortawesome/fontawesome-free-solid';
+import '@fortawesome/fontawesome-svg-core';
+
+
+const styles = (theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    grid: { //style for the grid component
+        "padding": "20px",
+        "margin-left": "0.5%",
+        "margin-right": "0.5%",
+        transform: 'translateZ(0)',
+        cursor: 'pointer',
+    },
+    media: { // style for the image in the card
+        height: "40%",
+        width: "100%",
+        // paddingTop: '56.25%', // 16:9
+    },
+    cardContent: { // Styles for the card content
+        "padding": "10px",
+        "margin-left": "20px",
+        "margin-right": "20px",
+        "height": "20%",
+        "display": "flex",
+        "align-items": "center",
+    },
+    cardActionArea: { //Style for the Card action area button
+        "display": "flex",
+        "height": "100%",
+        "flex-direction": "column",
+        "align-items": "normal",
+        "justify-content": "space-between",
+
+    },
+    gridCard: { //Style for the Grid card
+        '@media (min-width: 1200px)': { //Making the code responsive to different screens
+
+            'flex-grow': '0',
+            'max-width': '25%',
+            'flex-basis': '25%',
+        },
+
+        '@media (min-width: 960px) and (max-width:1200px)': { //Making the code responsive to different screens
+            'flex-grow': '0',
+            'max-width': '33%',
+            'flex-basis': '33%',
+        },
+    },
+
+    card: { //Style for the card and responsive code for different screen size
+        height: "500px",
+        '@media (min-width: 1300px)': { //Making the code responsive to different screens
+            height: "500px",
+        },
+        '@media (min-width: 960px) and (max-width:1300px)': { //Making the code responsive to different screens
+            height: "375px",
+        }
+    },
+
+    title: { //Style for the Title in the Card
+        "font-size": "25px",
+        '@media (min-width: 1300px)': {
+            "font-size": "40px",
+        },
+        '@media (min-width: 960px) and (max-width:1300px)': {
+            "font-size": "30px",
+        },
+        '@media (max-width: 960px)': {
+            "font-size": "40px",
+        }
+    },
+    categories: { //Style for the categories in the card
+        "font-size": "16px",
+        '@media (min-width: 1300px)': {
+            "font-size": "22px",
+        },
+        '@media (min-width: 960px) and (max-width:1300px)': {
+            "font-size": "20px",
+        },
+        '@media (max-width: 960px)': {
+            "font-size": "22px",
+        }
+    }
+}))
 
 class Home extends Component {
-  render() {
-    let displayRestaurants = this.props.displayRestaurants;
-    return (
-      <div>
-        <Header
-          baseUrl={this.props.baseUrl}
-          filterRestaurants={this.props.filterRestaurants}
-        />
-        {displayRestaurants.length > 0 ? (
-          <div>
-            {displayRestaurants.map((temp) => {
-              return <div key={temp.id}>{temp.restaurant_name}</div>;
-            })}
-          </div>
-        ) : (
-          <div>
-              No restaurant with the given name.
-          </div>
-        )}
-      </div>
-    );
-  }
+
+    restaurantCardClicked(id) {
+
+    }
+
+    render() {
+        let displayRestaurants = this.props.displayRestaurants;
+        const { classes } = this.props;
+        return (
+            <div>
+                <Header
+                    baseUrl={this.props.baseUrl}
+                    filterRestaurants={this.props.filterRestaurants}
+                />
+                <Grid container spacing={3} wrap="wrap" alignContent="center" className={classes.grid}>
+                    {displayRestaurants.length > 0 ? (
+                        <div>
+                            {displayRestaurants.map(restaurant => (
+                                <Grid key={restaurant.id} item xs={12} sm={6} md={3} className={classes.gridCard}>
+                                    <Card className={classes.card}>
+                                        <CardActionArea className={classes.cardActionArea} onClick={() => this.restaurantCardClicked(restaurant.id)}>
+                                            <CardMedia
+                                                className={classes.media}
+                                                image={restaurant.photo_URL}
+                                                title={restaurant.restaurant_name}
+                                            />
+                                            <CardContent className={classes.cardContent}>
+                                                <Typography className={classes.title} variant="h5" component="h2">
+                                                    {restaurant.restaurant_name}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardContent className={classes.cardContent}>
+                                                <Typography variant="subtitle1" component="p" className={classes.categories}>
+                                                    {restaurant.categories}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardContent className={classes.cardContent}>
+                                                <div className="card-bottom-info">
+                                                <span className="rest-rating">
+                                                    <span>
+                                                        <FontAwesomeIcon icon="star" size="lg" color="white" />
+                                                    </span>
+                                                    <Typography variant="caption" component="p" >{restaurant.customer_rating}</Typography>
+                                                    <Typography variant="caption" component="p" >({restaurant.number_customers_rated})</Typography>
+                                                </span>
+                                                    <span className="rest-for-two">
+                                                    <Typography variant="caption" component="p" style={{fontSize: '14px'}}>
+                                                        <i className="fa fa-inr" aria-hidden="true"></i>
+                                                        {restaurant.average_price}
+                                                    </Typography>
+                                                    <Typography variant="caption" component="p" style={{fontSize: '14px'}}>for two</Typography>
+                                                </span>
+                                                </div>
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </div>
+                    ) : (
+                        <div>
+                            No restaurant with the given name.
+                        </div>
+                    )}
+                </Grid>
+            </div>
+        );
+    }
+
+
 }
 
-export default Home;
+export default withStyles(styles)(Home);
