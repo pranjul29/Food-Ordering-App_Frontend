91061344dc343cd963993c3afe0a9232dcdbbd3f
Sundeep Prakash Jaiswal <sundeeppjaiswal@hotmail.com>
2021-02-16 15:22:20 +0530

    Checkout page and other changes.
    checkout page and other changes.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Controller from '../src/screens/Controller';
+import Routes from './screens';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+const theme = createMuiTheme({
+  typography: {
+    subtitle1: {
+      fontSize: 12,
+    },
+    body1: {
+      fontSize: 12,
+    },
+    h3: {
+      fontSize: '1.2rem',
+    },
+  },
+});
 
 ReactDOM.render(
-    <div>
-        <Controller />
-    </div>,
-    document.getElementById('root')
+  <div>
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </div>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
